f3d69cf8988a734f2537f5a2b1bb7e5c35b9b691
Lucia Pauw <25855123@sun.ac.za>
2024-10-18 14:38:37 +0200

    Remove unused generate_response

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -25,7 +25,7 @@ def get_text_message_input(recipient, text):
     )
 
 
-def generate_response(response):
+# def generate_response(response):
     # Return text in uppercase
     return response.upper()
 
